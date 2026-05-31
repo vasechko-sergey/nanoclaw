35e8577d0ca1697b60e2f5cf1fab5bcabc61c55d
Sergei Vasechko <sergei.vasechko@anna.money>
2026-05-31 17:34:22 +0800

    ios: pin Package.swift to iOS 16 + Swift 5.9 (match project.yml)

--- ios/JarvisApp/Package.swift
+++ ios/JarvisApp/Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "JarvisApp",
-    platforms: [.iOS(.v18)],
+    platforms: [.iOS(.v16)],
     dependencies: [
         .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.0"),
     ],
