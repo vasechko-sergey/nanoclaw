a601b028c6b9ac0c6e599e27457a46d69067bdd1
Sergei Vasechko <sergei.vasechko@anna.money>
2026-05-31 17:33:31 +0800

    ios: add GRDB dependency for local SQLite store

--- ios/JarvisApp/Package.swift
+++ ios/JarvisApp/Package.swift
@@ -1,10 +1,19 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "JarvisApp",
     platforms: [.iOS(.v18)],
+    dependencies: [
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.0"),
+    ],
     targets: [
-        .executableTarget(name: "JarvisApp", path: "Sources/JarvisApp")
+        .executableTarget(
+            name: "JarvisApp",
+            dependencies: [
+                .product(name: "GRDB", package: "GRDB.swift"),
+            ],
+            path: "Sources/JarvisApp"
+        )
     ]
 )
